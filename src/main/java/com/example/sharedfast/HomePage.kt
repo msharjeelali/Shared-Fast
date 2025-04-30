65581a5bae1f74010c8b0aaacbe7d71e03d71e2c
Muhammad Sharjeel Ali <msharjeelali@yahoo.com>
2025-05-01 02:01:20 +0500

    Splash screen created

---
+++ src/main/java/com/example/sharedfast/HomePage.kt
@@ -0,0 +1,17 @@
+package com.example.sharedfast
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class HomePage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        setContentView(R.layout.activity_home_page)
+
+    }
+}
--- src/main/java/com/example/sharedfast/MainActivity.kt
+++ src/main/java/com/example/sharedfast/MainActivity.kt
@@ -1,47 +1,23 @@
 package com.example.sharedfast
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.sharedfast.ui.theme.SharedFastTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            SharedFastTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+        setContentView(R.layout.activity_splash_screen)
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SharedFastTheme {
-        Greeting("Android")
+        val myButton: Button = findViewById(R.id.intentButton)
+        myButton.setOnClickListener({
+            startActivity(Intent(this, HomePage::class.java))
+        })
+
     }
 }
